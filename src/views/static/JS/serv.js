cf6988cb680a33f1c9825cb91444fd6e7d383d84
ryuklyra <karenlisethduquegomez@hotmail.com>
2025-04-20 10:18:39 -0500

    ajustes
    ajustes de detalles de interfaz

--- src/views/static/JS/serv.js
+++ src/views/static/JS/serv.js
@@ -29,3 +29,22 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     });
 });
+
+function abrirModal(id) {
+    document.getElementById(id).style.display = 'block';
+  }
+
+  function cerrarModal(id) {
+    document.getElementById(id).style.display = 'none';
+  }
+
+  // Cierra el modal si se hace clic fuera de él
+  window.onclick = function(event) {
+    const modales = document.getElementsByClassName('modal');
+    for (let i = 0; i < modales.length; i++) {
+      if (event.target === modales[i]) {
+        modales[i].style.display = 'none';
+      }
+    }
+  };
+
